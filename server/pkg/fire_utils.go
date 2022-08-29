cfb9d8cc644bd5251709cb7b1b1071a4213f9c80
Ismael Shakverdiev <me@theiskaa.com>
2022-08-29 15:18:01 +0400

    feat: add VerifyFireToken function to server

--- server/pkg/fire_utils.go
+++ server/pkg/fire_utils.go
@@ -2,9 +2,26 @@ package pkg
 
 import (
 	"context"
+	"strings"
 
 	"cloud.google.com/go/firestore"
+	"firebase.google.com/go/auth"
 )
+
+// VerifyFireToken  verifies if passed token is valid or not.
+// If it is, returns the owner user's user id.
+// If not, returns the [Unauthorized] error.
+func VerifyFireToken(token string, auth *auth.Client) (*string, *AppError) {
+	ctx := context.Background()
+
+	t := strings.ReplaceAll(token, "Bearer ", "")
+	res, er := auth.VerifyIDToken(ctx, t)
+	if er != nil {
+		return nil, &Unauthorized
+	}
+
+	return &res.UID, nil
+}
 
 // Gets concrete collections' concrete data (as map).
 func GetDocumentsData(collection firestore.CollectionRef, id string) (res map[string]interface{}, err error) {
