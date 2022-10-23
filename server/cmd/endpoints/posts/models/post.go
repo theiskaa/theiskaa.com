9eb4a7aafb651a13382371c10b8ea43d52d3fbdc
Ismael Shakverdiev <me@theiskaa.com>
2022-10-23 23:30:25 +0400

    feat: re-implement the post model structures

--- server/cmd/endpoints/posts/models/post.go
+++ server/cmd/endpoints/posts/models/post.go
@@ -1,7 +1,7 @@
 //
-// This source code is distributed under the terms of Bad Code License.
-// You are forbidden from distributing software containing this code to
-// end users, because it is bad.
+// Copyright 2022-present theiskaa. All rights reserved.
+// Use of this source code is governed by Apache-2.0 license
+// that can be found in the LICENSE file.
 //
 
 package posts
@@ -12,17 +12,64 @@ import (
 
 // Post is the main model structure of the posts endpoint.
 type Post struct {
-	ID          string `json:"id"`          // A unique identification string
-	Title       string `json:"title"`       // A descriptive title
-	Description string `json:"description"` // A short description
-	Cover       string `json:"picture"`     // A relevant cover photo
-	Date        string `json:"date"`        // The posting || updating date
-	Content     string `json:"content"`     // The body, in Markdown or HTML
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Cover       string `json:"picture"`
+	Date        string `json:"date"`
+	Content     []Href `json:"content"`
 }
 
-// Converts the [Post] data to the map.
+// The content token reference model.
+// Which is a dynamic structure that could represent:
+// - text(normal/linked)
+// - image
+// - code-snippet
+//
+// Each that representation should be handled by [Href]'s [Type].
+// And [Src] of [Href] is the main source of value.
+type Href struct {
+	// Type is a field that represents the rendering style of [Href].
+	//
+	// Could be:
+	//  - text
+	//  - image
+	//  - code
+	Type string `json:"type"`
+
+	// Src is a field that used as source of content that has to be rendered.
+	Src string `json:"src"`
+
+	// Style is a font-style identifier of [Src] field.
+	// > In case of [Type] being any kind of text type.
+	//
+	// Could be:
+	//  - bold
+	//  - italic
+	//  - strong
+	Style string `json:"style,omitempty"`
+
+	// The reference URL provider for [Src].
+	// > In case of [Type] being linked text type. <text>.
+	//
+	// same approach of `<a href="http://">{Src}</a>`
+	// but in go structure model.
+	URL string `json:"url,omitempty"`
+}
+
+// ToJson converts the [Post] structure to map value.
 func (p *Post) ToJSON() map[string]interface{} {
 	res, _ := json.Marshal(p)
+
+	m := make(map[string]interface{})
+	json.Unmarshal(res, &m)
+
+	return m
+}
+
+// ToJson converts the [Href] structure to map value.
+func (h *Href) ToJSON() map[string]interface{} {
+	res, _ := json.Marshal(h)
 
 	m := make(map[string]interface{})
 	json.Unmarshal(res, &m)
