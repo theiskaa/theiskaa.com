d9d421999590e96d392f629cd9b818ca7a95d17e
Ismael Shakverdiev <me@theiskaa.com>
2022-10-28 20:47:29 +0400

    feat: impl servies & models
    - Added new structured error model, info model and link model.
    - Added new structured services:
       - Dio (the requests manager)
       - InfoService (custom dio wrapper implementation for info data
         management)

---
+++ web/src/models/info.rs
@@ -0,0 +1,50 @@
+//
+// Copyright 2022-present theiskaa. All rights reserved.
+// Use of this source code is governed by Apache-2.0 license
+// that can be found in the LICENSE file.
+//
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+pub struct InfoModel {
+    pub picture: String,
+    pub greeting: Vec<Link>,
+    pub career: Vec<Link>,
+    pub contact: Vec<Link>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+pub struct Link {
+    // Title is the main domain of [Link] structure.
+    pub title: String,
+
+    // The reference URL provider for [title].
+    // same approach of `<a href="http://">{Title}</a>`
+    // but in go structure model.
+    pub url: String,
+
+    // Style is a font-style identifier of [Title] field.
+    // Could be:
+    //  - bold
+    //  - italic
+    //  - strong
+    pub style: String,
+}
+
+impl InfoModel {
+    pub fn to_string(&self) -> String {
+        format!(
+            "
+            picture: {},
+            greeting: {},
+            career: {},
+            contact: {},
+        ",
+            self.clone().picture,
+            self.clone().greeting.len(),
+            self.clone().career.len(),
+            self.clone().contact.len(),
+        )
+    }
+}
---
+++ web/src/services/mod.rs
@@ -0,0 +1,11 @@
+//
+// Copyright 2022-present theiskaa. All rights reserved.
+// Use of this source code is governed by Apache-2.0 license
+// that can be found in the LICENSE file.
+//
+
+mod dio;
+mod info_service;
+
+pub use dio::Dio;
+pub use info_service::InfoService;
