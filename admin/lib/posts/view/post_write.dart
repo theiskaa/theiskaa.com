1cf9bc8e54274fd92166a4f87ab213f5f46f0b4f
Ismael Shakverdiev <me@theiskaa.com>
2022-12-28 23:05:18 +0400

    feat: add URL validator to post write screen

--- admin/lib/posts/view/post_write.dart
+++ admin/lib/posts/view/post_write.dart
@@ -11,6 +11,7 @@ import 'package:admin/widgets/fields.dart';
 import 'package:admin/posts/view/widgets/html_tag_editor.dart';
 import 'package:admin/widgets/loadings.dart';
 import 'package:admin/widgets/utils.dart';
+import 'package:admin/core/exts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -182,11 +183,7 @@ class _PostWriteState extends State<PostWrite> {
                     formKey: coverKey,
                     controller: coverController,
                     validator: (v) {
-                      final url = RegExp(
-                        r'(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+',
-                      );
-
-                      if (url.hasMatch(v ?? '')) return null;
+                      if ((v ?? '').isURL()) return null;
                       return "Invalid URL";
                     },
                   ),
