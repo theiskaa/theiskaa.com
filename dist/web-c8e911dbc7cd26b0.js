954131fa515bfa35fa0cd1d00aaa71e09eaa04bd
Ismael Sh <me@theiskaa.com>
2024-09-08 01:28:33 +0400

    feat(me): add details text and remodify css

--- dist/web-c8e911dbc7cd26b0.js
+++ dist/web-c8e911dbc7cd26b0.js
@@ -651,7 +651,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('web-c6ec9561ad88e477_bg.wasm', import.meta.url);
+        input = new URL('web-c8e911dbc7cd26b0_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
