6e50c46d46d6fe6e219525181cdeda1c2041f8db
Ismael Sh <me@theiskaa.com>
2024-09-24 22:35:11 +0400

    feat(home): update information texts

--- dist/web-b0ac1a5a11b63bf9.js
+++ dist/web-b0ac1a5a11b63bf9.js
@@ -603,11 +603,11 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper346 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper347 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 149, __wbg_adapter_20);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper663 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper664 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 255, __wbg_adapter_23);
         return addHeapObject(ret);
     };
@@ -651,7 +651,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('web-dfe3de73597cb3b5_bg.wasm', import.meta.url);
+        input = new URL('web-b0ac1a5a11b63bf9_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
--- src/routes/home.rs
+++ src/routes/home.rs
@@ -13,8 +13,10 @@ pub fn home() -> Html {
                             <p class="role">
                                 {"founder "}
                                 <a href="https://insolite.io" alt="insolite">{"insolite.io"}</a>
-                                {" founding member of "}
-                                <a href="https://lite.jobia.work" alt="jobia">{"lite.jobia.work"}</a>
+                                {". founding member of "}
+                                <a href="https://jobia.work" alt="jobia">{"jobia.work"}</a>
+                                {". engineer @"}
+                                <a href="https://tnet.ge" alt="tnet">{"tnet.ge"}</a>
                             </p>
                         </div>
                     </div>
@@ -34,54 +36,47 @@ pub fn home() -> Html {
                             {"I'm a self-taught software engineer who started programming at 13. With over a decade of experience, I began working professionally at 15, and now, at 19 years old, I've gained hands-on expertise across various startups and projects."}
                         </p>
                         <p>
-                            {"Since my early start, I’ve contributed to multiple innovative open-source initiatives. Alongside my startup at Insolite, we've released 8 Flutter/Dart packages, which can be found "}
-                            <a href="https://pub.dev/publishers/insolite.io/packages" alt="insolite pub.dev">{"at pub.dev of insolite"}</a>
-                            {" or "}
-                            <a href="https://pub.dev/publishers/theiskaa.com/packages" alt="my pub.dev">{"my personal pub.dev"}</a>
-                            {"."}
+                          {"Since my early start, I’ve contributed to several innovative open-source projects. Together with my startup, Insolite, we’ve released 8 Flutter/Dart "}
+                          <a href="https://pub.dev/publishers/insolite.io/packages" alt="insolite pub.dev">{"packages"}</a>
+                          {". I also tinker with some personal projects, most of them are open source and available on my "}
+                          <a href="https://github.com/theiskaa" alt="github.com/thiskaa">{"github"}</a>
+                          {" profile."}
                         </p>
                         <p>
-                            {"Here is a small list of my open source projects:"}
+                            {"I spend a large part of my free time thinking about consciousness, low level programming, catching up on papers about artificial intelligence & building intuition on math fundamentals."}
                         </p>
-                        <ul>
-                            <li><a href="https://pub.dev/packages/hidable" alt="Hidable">{"Hidable"}</a> {" (Flutter/Dart) - A Flutter widget for scroll-to-hide functionality."}</li>
-                            <li><a href="https://pub.dev/packages/field_suggestion" alt="Field Suggestion">{"Field Suggestion"}</a> {" (Flutter/Dart) - A lightweight, customizable search field for Flutter."}</li>
-                            <li><a href="https://pub.dev/packages/widget_slider" alt="Widget Slider">{"Widget Slider"}</a> {" (Flutter/Dart) - A fast and user-friendly animated slider list widget for Flutter."}</li>
-                            <li><a href="https://github.com/insolite-dev/notya" alt="Notya">{"Notya"}</a> {" (Go, Flutter/Dart, Firebase) - A versatile command-line notes app with cross-platform mobile integration."}</li>
-                            <li><a href="https://github.com/theiskaa/replace" alt="Replace">{"Replace"}</a> {" (C) - A versatile tool that enables you to perform efficient replacement operations within specified files, with a recursive replacement algorithm."}</li>
-                            <li><a href="https://github.com/theiskaa/mate" alt="Mate">{"Mate"}</a> {" (Rust) - A powerful arithmetic expression interpreter and calculator in Rust, published on Crates.io."}</li>
-                            <li><a href="https://github.com/theiskaa/hivetime" alt="Hivetime">{"Hivetime"}</a> {" (Rust) - A straightforward interpreter and calculator designed for short-time syntax, primarily used in Jira work logs."}</li>
-                            <li><a href="https://github.com/theiskaa/mdPdf" alt="mdPdf">{"mdPdf"}</a> {" (Rust) - A simple tool and library to create/transpile Markdown to PDF."}</li>
-                        </ul>
-
                         <h2>{"Past Work"}</h2>
                         <p>
-                            {"From 2023 to 2024, I worked as a Software Engineer at "}
-                            <a href="https://www.l3vels.xyz/" alt="L3VELS">{"L3VELS"}</a>
-                            {" and later at "}
-                            <a href="https://datura.ai" alt="Datura.ai">{"Datura.ai"}</a>
-                            {", where my team and I contributed to cutting-edge AI platforms, including "}
-                            <a href="https://github.com/l3vels/L3AGI" alt="L3AGI">{"l3agi"}</a>
-                            {" and "}
-                            <a href="https://datura.ai" alt="Datura">{"Datura"}</a>
-                            {" (Bittensor’s Subnet 22). My work primarily focused on backend development and AWS infrastructure, including Lambda, SQS, and CloudFormation. I played an integral role in the configuration and optimization of systems for miners and validators, ensuring scalability and efficiency. Additionally, I led the development of an advanced image categorization tool similar to Google Photos' AI-powered search, enhancing the platform's search capabilities and user experience."}
+                          {"From October 2023 to September 2024, I worked as a Software Engineer at "}
+                          <a href="https://www.l3vels.xyz/" alt="L3VELS">{"L3VELS"}</a>
+                          {" and later at "}
+                          <a href="https://datura.ai" alt="Datura.ai">{"Datura.ai"}</a>
+                          {", where my team and I contributed to cutting-edge AI platforms, including "}
+                          <a href="https://github.com/l3vels/L3AGI" alt="L3AGI">{"L3AGI"}</a>
+                          {" and "}
+                          <a href="https://datura.ai" alt="Datura">{"Datura"}</a>
+                          {" (Bittensor’s Subnet 22). My work primarily focused on backend development and AWS infrastructure, such as Lambda, SQS, and CloudFormation. I played a key role in configuring and optimizing systems for miners and validators to ensure scalability and efficiency. I also led the development of an advanced image categorization tool, similar to Google Photos' AI-powered search, which significantly enhanced the platform's search capabilities and user experience, and more."}
                         </p>
                         <p>
-                            {"Before that, I served as a Senior Mobile Engineer at "}
-                            <a href="https://zencode.io" alt="Zencode">{"Zencode"}</a>
-                            {", where I worked on fintech projects. I developed a highly secure local authentication system, resistant to reverse engineering attacks, safeguarding user data at every level. I also built an in-app chat system with real-time communication features and integrated Firebase Cloud Messaging for seamless and lag-free notifications. Moreover, I implemented deep linking strategies by bridging native Android/iOS code with Flutter, which enhanced the app’s functionality and user experience across platforms."}
+                          {"Before that, I was a Senior Software Engineer at "}
+                          <a href="https://zencode.io" alt="Zencode">{"Zencode"}</a>
+                          {", where I worked with the mobile engineering team on "}
+                          <a href="https://payme.zencode.io" alt="Zenbank">{"Payme"}</a>
+                          {", an internet bank. I developed a highly secure local authentication system, resistant to reverse engineering, ensuring user data was protected at every level. I built an in-app chat system with real-time communication features, integrated Firebase Cloud Messaging for smooth and instant notifications, designed the entire routing structure and deep linking strategies by bridging native Android/iOS code with Flutter, which improved the app’s functionality and user experience across platforms, and contributed to many other"}
                         </p>
                         <p>
-                            {"As a founding member of "}
-                            <a href="https://lite.jobia.work" alt="Jobia">{"Jobia"}</a>
-                            {", I am responsible for all technical aspects, from building the application from the ground up, implementing specific algorithms to match users to vacancies based on their soft skills and interests, managing teams, to deploying the application. Every issue or problem on the website or mobile application is ultimately my responsibility."}
+                          {"Currently, as a founding member of "}
+                          <a href="https://jobia.work" alt="Jobia">{"Jobia"}</a>
+                          {", I am responsible for all technical aspects—building the application from the ground up, implementing complex algorithms to match users with vacancies based on their soft skills and interests, managing engineering teams, and more. Any issue or problem with the website or mobile application ultimately falls under my responsibility."}
+                          <br/>
+                          {" Previously, I was a Senior Software Engineer at Jobia and a core member of the team, overseeing web, mobile, backend, DevOps, and more. Unfortunately, Jobia went bankrupt, unable to secure further investment, and the startup shut down."}
                         </p>
                         <p>
-                            {"During my internship at "}
-                            <a href="https://lomsa.com" alt="Lomsa">{"Lomsa"}</a>
-                            {", I was responsible for designing and developing core pages for their mobile app, such as the homepage, login page, and settings page. I implemented essential API functionality, including creating custom API keys and managing WebSocket connections to handle concurrent users. I also contributed to their open-source project, "}
-                            <a href="https://github.com/lomsa-dev/http-mock-adapter" alt="http_mock_adapter">{"http_mock_adapter"}</a>
-                            {", a popular HTTP testing tool that streamlines API testing for developers."}
+                          {"During my internship at "}
+                          <a href="https://lomsa.com" alt="Lomsa">{"Lomsa"}</a>
+                          {", I was responsible for designing and developing core pages for their mobile application. I implemented essential API functionality, including creating custom API keys and managing WebSocket connections to handle concurrent processes. I also contributed to their open-source project, "}
+                          <a href="https://github.com/lomsa-dev/http-mock-adapter" alt="http_mock_adapter">{"http_mock_adapter"}</a>
+                          {", a popular HTTP testing tool that simplifies API testing for developers. Additionally, I worked on creating RenderBox-based widgets, including nested widget grouping and folder structures."}
                         </p>
                     </div>
 
@@ -89,9 +84,10 @@ pub fn home() -> Html {
 
                     <div class="bio">
                         <p>
-                            {"My journey from a programming enthusiast to a professional developer, now at almost 19 years old, has shaped me into a versatile engineer with a passion for solving problems and building tools that make a difference. If you’d like to collaborate or learn more, feel free to reach out at "}
+                            {"Feel free to reach out at "}
                             <a href="mailto:me@theiskaa.com" alt="email">{"me@theiskaa.com"}</a>
-                            {"."}
+                            {" or find me on"}
+                            <a href="https://x.com/theiskaa" alt="email">{"@theiskaa"}</a>
                         </p>
                     </div>
                 </div>
