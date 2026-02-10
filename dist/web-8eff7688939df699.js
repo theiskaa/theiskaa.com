6ece40ac31959ddefc37ead64bc27bda0de04afd
Ismael Sh <me@theiskaa.com>
2026-02-11 00:17:36 +0400

    fix(posts): html_highlight github link

--- dist/web-8eff7688939df699.js
+++ dist/web-8eff7688939df699.js
@@ -671,7 +671,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('web-69d1a1605297235b_bg.wasm', import.meta.url);
+        input = new URL('web-8eff7688939df699_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
