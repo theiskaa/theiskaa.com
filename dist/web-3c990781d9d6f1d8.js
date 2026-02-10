4e0a8eb888e5c293f96da522110308f2ae4d875a
Ismael Sh <me@theiskaa.com>
2026-02-11 00:31:36 +0400

    fix(posts): type on debris drawing article

--- dist/web-3c990781d9d6f1d8.js
+++ dist/web-3c990781d9d6f1d8.js
@@ -671,7 +671,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('web-8eff7688939df699_bg.wasm', import.meta.url);
+        input = new URL('web-3c990781d9d6f1d8_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
